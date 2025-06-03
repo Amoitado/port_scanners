8cddd2749684fad4719794b6d15108663861fb90
Amoitado <aalanricardoo@gmail.com>
2025-06-02 23:05:33 -0300

    Python portscan v1.0

--- python_portscan.py
+++ python_portscan.py
@@ -1,12 +1,26 @@
 import socket
+import sys
 
+def scan(host, ports):
+    try:
+        for port in ports:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(0.5)
+            code = client.connect_ex((host, int(port)))
+            if code == 0:
+                print("[+] {} open".format(port));
 
-host = "google.com"
-ports = range(100)
+    except:
+        print("Error, something is wrong.")
 
-for port in ports:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.5)
-    code = client.connect_ex((host, port))
-    if code == 0:
-        print("[+] {} open".format(port));
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        host = sys.argv[1]
+        if len(sys.argv) >=3:
+            ports = sys.argv[2].split(",")
+        else:
+            ports = range(400)
+
+        scan(host, ports)
+    else:
+        print("Usage: python3 portscan.py <domain> <port>,<port>,<port>... ")
