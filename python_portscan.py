02420e53e8da5b54d81efb6694949c82a4cfe41a
Amoitado <aalanricardoo@gmail.com>
2025-06-02 22:41:12 -0300

    Initializing prototype

---
+++ python_portscan.py
@@ -0,0 +1,12 @@
+import socket
+
+
+host = "google.com"
+ports = range(100)
+
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.5)
+    code = client.connect_ex((host, port))
+    if code == 0:
+        print("[+] {} open".format(port));
